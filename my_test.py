2eb7002cf501861713f38b68ea6755a8c49d124a
Grazfather <grazfather@gmail.com>
2015-03-08 17:48:15 -0700

    Add support for hw breakpoints

--- my_test.py
+++ my_test.py
@@ -1,12 +1,13 @@
 import my_debugger
+from my_debugger_defines import *
 
 debugger = my_debugger.debugger()
 
 pid = raw_input("Enter the PID of the process to attach to: ")
 debugger.attach(int(pid))
-printf_address = debugger.func_resolve("msvcrt.dll", "printf")
 
+printf_address = debugger.func_resolve("msvcrt.dll", "printf")
 print "[*] Address of printf: 0x%08x" % printf_address
 
-debugger.bp_set(printf_address)
+debugger.bp_set_hw(printf_address, 1, HW_EXECUTE)
 debugger.run()
