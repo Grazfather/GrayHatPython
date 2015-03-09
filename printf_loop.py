343833065ebc023d93d42e6e9658d1be8b47ec0a
Grazfather <grazfather@gmail.com>
2015-03-08 21:50:08 -0700

    Add pydbg bp handler example. Fix printf_loop
    Because printf_loop was creating the string before passing it to printf
    there was no chance to modify the argument to the format string (since
    there was no format string.) This is another mistake from the book that
    was only fixed in the source code online.

--- printf_loop.py
+++ printf_loop.py
@@ -5,6 +5,6 @@ msvcrt = cdll.msvcrt
 counter = 0
 
 while 1:
-    msvcrt.printf("Loop iteration %d\n" % counter)
+    msvcrt.printf("Loop iteration %d\n", counter)
     time.sleep(1)
     counter += 1
